c27c8cc596fa65f58642be4f01b271e9d62f80a0
parsariyahi <pany.parsariyahi@gmail.com>
2024-02-24 10:23:53 +0330

    tests: Add test for ``Decoder``

---
+++ tests/translator/test_decoder.py
@@ -0,0 +1,10 @@
+from morsep import Decoder
+
+
+def test_decode_morse_code_to_string():
+    morse_code = "0000.0.0100.0100.111/011.111.010.0100.100"
+    morse_code_string = "hello world"
+    decoder = Decoder()
+    decoded = decoder.decode(morse_code)
+
+    assert decoded == morse_code_string
