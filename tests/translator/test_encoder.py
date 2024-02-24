d552c9de228fce16a79f7f9850d4c1d6813943ee
parsariyahi <pany.parsariyahi@gmail.com>
2024-02-24 10:23:34 +0330

    tests: Add test for ``Encoder``

---
+++ tests/translator/test_encoder.py
@@ -0,0 +1,10 @@
+from morsep import Encoder
+
+
+def test_encode_string_to_morse_code():
+    string = "hello world"
+    string_morse_code = "0000.0.0100.0100.111/011.111.010.0100.100"
+    encoder = Encoder()
+    encoded = encoder.encode(string)
+
+    assert encoded == string_morse_code
