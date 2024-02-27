d629c936a3ac511f3145037779368af66593fe2e
parsariyahi <pany.parsariyahi@gmail.com>
2024-02-27 19:23:11 +0330

    feature: Add asyncio and reading data over socket wihtout blocking loops

--- morsep/_server/server.py
+++ morsep/_server/server.py
@@ -1,24 +1,29 @@
-from typing import Optional
+import asyncio
 import socket
+from typing import Optional
+
+from .handler import RequestHandler
 
 class Server:
     def __init__(self, host: Optional[str] = None, port: Optional[int] = 8555) -> None:
         self.host = host if host else socket.gethostname()
         self.port = port
-        self._prepare_socket()
 
     def start(self):
-        self.socket.listen()
-        conn, addr = self.socket.accept()
+        asyncio.run(self._prepare_server())
+
+    async def _prepare_socket(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((self.host, self.port))
+        self.socket.listen(1)
+        self.socket.setblocking(False)
+
+    async def _prepare_server(self):
+        await self._prepare_socket()
+
+        loop = asyncio.get_event_loop()
 
         while True:
-            data = conn.recv(1024).decode("utf-8")
-            print(data)
-            if not data:
-                break
-        
-        conn.close()
-
-    def _prepare_socket(self):
-        self.socket = socket.socket()
-        self.socket.bind((self.host, self.port))
+            request, _ = await loop.sock_accept(self.socket)
+            handler = RequestHandler(request)
+            loop.create_task(handler.get_data())
