12e3f43a40a1087003a9b8f46c9c1e47825a65fc
parsariyahi <pany.parsariyahi@gmail.com>
2024-02-28 12:02:20 +0330

    feature: Add simple test response for client

--- morsep/_server/handler.py
+++ morsep/_server/handler.py
@@ -1,6 +1,7 @@
 import asyncio
 import socket
 
+from .._translator.decoder import Decoder
 
 class RequestHandler:
     def __init__(self, request: socket.socket) -> None:
@@ -9,6 +10,10 @@ class RequestHandler:
     async def get_data(self):
         loop = asyncio.get_event_loop()
         data = (await loop.sock_recv(self.request, 255)).decode("utf-8")
+        await loop.sock_sendall(self.request, "I get that thanks".encode("utf-8")) #Just for testing the reponse part of the request.
         self.request.close()
-        print(data)
+
+        d = Decoder()
+        decoded_data = d.decode(data)
+        print("raw data: ", data, "\ndecoded data: ", decoded_data)
         return data
