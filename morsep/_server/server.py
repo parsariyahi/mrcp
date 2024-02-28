1f322da882f0e194ad71e25d497661e31968892f
parsariyahi <pany.parsariyahi@gmail.com>
2024-02-28 12:01:47 +0330

    feature: Add listen for server and better event loop handling

--- morsep/_server/server.py
+++ morsep/_server/server.py
@@ -10,20 +10,31 @@ class Server:
         self.port = port
 
     def start(self):
-        asyncio.run(self._prepare_server())
+        loop = asyncio.get_event_loop()
+        self._prepare_socket()
+        loop.create_task(self.listen())
+        loop.run_forever()
+        
+    async def _main(self):
+        loop = asyncio.get_event_loop()
+        loop.create_task(self._prepare_server())
 
-    async def _prepare_socket(self):
+    def _prepare_socket(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind((self.host, self.port))
-        self.socket.listen(1)
+        self.socket.listen(10)
         self.socket.setblocking(False)
 
-    async def _prepare_server(self):
-        await self._prepare_socket()
-
+    async def listen(self):
+        print("running listen server \n")
         loop = asyncio.get_event_loop()
 
         while True:
             request, _ = await loop.sock_accept(self.socket)
             handler = RequestHandler(request)
             loop.create_task(handler.get_data())
+
+    async def get_all_tasks(self):
+        print("get all task running \n")
+        for task in asyncio.all_tasks():
+            print(task.print_stack())
