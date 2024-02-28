523475757622a23bc2842e2a7e5ce64ea7137715
parsariyahi <pany.parsariyahi@gmail.com>
2024-02-28 12:02:42 +0330

    feature: Add ``get_response`` method

--- morsep/_client/client.py
+++ morsep/_client/client.py
@@ -1,6 +1,8 @@
 from typing import Optional
 import socket
 
+from .._translator.encoder import Encoder
+
 class Client:
     def __init__(self, host: str, port: Optional[int] = 8555) -> None:
         self.host = host
@@ -12,5 +14,11 @@ class Client:
         self.socket = socket.socket()
         self.socket.connect((self.host, self.port))
 
-    def request(self, data):
-        self.socket.send(data)
+    def request(self, data: str):
+        e = Encoder()
+        data = e.encode(data)
+        self.socket.send(data.encode("utf-8"))
+
+    def get_response(self):
+        data = self.socket.recv(255)
+        print(data)
