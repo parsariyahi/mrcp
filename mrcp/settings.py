bfe34e42207d75101d981aa17a53c14e2e35d65f
parsariyahi <pany.parsariyahi@gmail.com>
2024-03-11 23:01:09 +0330

    feature: Add ``port``, ``host`` defaults into settings

--- mrcp/settings.py
+++ mrcp/settings.py
@@ -1,6 +1,11 @@
+import socket
+
 class Settings:
     # The chunk size with "\n" is 50,
     # So when we want to send the cunk,
     # We need to send 49 bit,
     # One bit is reserved for "\n".
     CHUNK_SIZE_BIT_SIZE = 50
+    
+    DEFAULT_PORT = 8555
+    DEFAULT_HOST = socket.gethostname()
